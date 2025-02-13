414f216f43fc3a22a8146a2f07707ce6bd6eb88c
gerdesjasonl <gerdesjasonl@gmail.com>
2025-02-13 08:06:48 -0500

    This finishes TODO for server/src/middleware/auth.ts to authenticate JWT tokens.

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,17 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+const authHeader = req.headers.authorization;
+if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  return res.status(401).json({ message: 'Denied Token' });
+}
+const token = authHeader.split(' ')[1];
+
+try {
+  const decoded =jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+  req.user = decoded;
+  next();
+} catch (error) {
+  return res.status(403).json({ message: 'Token Invalid' });
+}
 };
