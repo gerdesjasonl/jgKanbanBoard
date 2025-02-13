eba57170c8fc4aea697815890d5f326e9eadd276
gerdesjasonl <gerdesjasonl@gmail.com>
2025-02-13 08:46:32 -0500

    TODOs for client/src/utils/auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,45 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    if (!token) return true;
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (!decoded.exp) return true;
+      return decoded.exp < Date.now() / 1000;
+    } catch (error) {
+      return true;
+    }
   }
 
-  getToken(): string {
+  getToken(): string | null {
     // TODO: return the token
+    return localStorage.getItem('id_token');
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.href = '/login';
   }
 }
 
