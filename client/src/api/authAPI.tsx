b90a81c33e7c0a74ad3a64bab4c2713f25a295b3
gerdesjasonl <gerdesjasonl@gmail.com>
2025-02-13 08:25:55 -0500

    Completes TODO to make POST request in client/src/api/authAPI.tsx

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,24 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+try {
+  const response = await fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userInfo),
+  });
+  if (!response.ok) {
+    throw new Error('Login Failed');
+  }
+
+  const data = await response.json();
+  return data.token;
+} catch (error) {
+  console.error('Login Error', error);
+  return null;
+}
 }
 
 
